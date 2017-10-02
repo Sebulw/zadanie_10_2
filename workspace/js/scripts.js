4f5ab20c647d8270dbb5153bf803e73e8f117cec
Sebastian Wojnicz <sebulw@gmail.com>
2017-10-02 21:44:06 +0200

    script done

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -6,9 +6,11 @@ var span = $("span");
 });
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
-    var button = '<button class="btn data-tmp="' +index + '">Click me</button>';
+    var button = '<button class="btn" data-tmp="' +index + '">Click me</button>';
     $(element).append(button);
 });
 $('button').click(function() {
+    console.log($(this));
+    console.log($(this).attr('data-tmp'));
     alert($(this).attr('data-tmp'));
 });
